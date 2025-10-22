f3d81f27401505dad622c1aaa265237ca7140dac
anism74 <91490601+anism74@users.noreply.github.com>
2025-10-22 10:09:49 +0200

    Rename constructor parameters in Fenetre class
    Updated constructor parameter names for clarity.

--- Fenetre.py
+++ Fenetre.py
@@ -1,10 +1,11 @@
 import random
 
 class Fenetre:
-    def __init__(self, nom: str, borne_min: float, borne_max: float):
-        self.nom = nom
-        self.borne_min = borne_min
-        self.borne_max = borne_max
+    """Représente une fenêtre de recherche [borne_min, borne_max] pour une variable donnée."""
+    def __init__(self, anom: str, aborne_min: float, aborne_max: float):
+        self.nom = anom
+        self.borne_min = aborne_min
+        self.borne_max = aborne_max
 
     def generer_valeur(self) -> float:
         "Retourne une valeur aléatoire dans la fenêtre."
@@ -17,6 +18,3 @@ class Fenetre:
     def __repr__(self):
         "Affichage des bornes"
         return f"Fenetre({self.nom}: [{self.borne_min}, {self.borne_max}])"
-
-
-"test"
