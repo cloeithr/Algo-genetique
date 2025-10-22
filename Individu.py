a3dd5060c6665ebaa10a5b5c287988be716c92c6
CLOE INTIHAR <cloeintihar24@gmail.com>
2025-10-22 10:20:48 +0200

    modif methodes

--- Individu.py
+++ Individu.py
@@ -14,17 +14,40 @@ class Individu:
         return self.performance 
   
     def muter(self, taux_mutation):
-        # Si codage binaire présent, muter le binaire
         if self.codage and self.binaire:
+            # Mutation sur le code binaire
             self.binaire = self.codage.mutation(self.binaire, taux_mutation)
+            # Mettre à jour les coordonnées réelles
             self.coordonnees = self.codage.decoder(self.binaire)
         else:
-            # Mutation classique sur les réels
+            # Mutation classique sur les coordonnées réelles
             for i, (xmin, xmax) in enumerate(self.fenetres):
                 if random.random() < taux_mutation:
                     amplitude = (xmax - xmin) * 0.1
                     self.coordonnees[i] += random.uniform(-amplitude, amplitude)
                     self.coordonnees[i] = max(min(self.coordonnees[i], xmax), xmin)
+                    
+    def croiser(self, parent1, parent2):
+        if parent1.codage and parent1.binaire and parent2.binaire:
+            # Croisement sur le code binaire
+            enfant1_binaire, enfant2_binaire = parent1.codage.croiser(parent1.binaire, parent2.binaire)
+            e1 = Individu(parent1.codage.decoder(enfant1_binaire), parent1.fenetres, parent1.codage)
+            e1.binaire = enfant1_binaire
+            e2 = Individu(parent2.codage.decoder(enfant2_binaire), parent2.fenetres, parent2.codage)
+            e2.binaire = enfant2_binaire
+            return e1, e2
+        else:
+            # Croisement sur les coordonnées réelles (ton code actuel)
+            enfant1_coord, enfant2_coord = [], []
+            for x1, x2 in zip(parent1.coordonnees, parent2.coordonnees):
+                if random.random() < 0.5:
+                    enfant1_coord.append(x1)
+                    enfant2_coord.append(x2)
+                else:
+                    enfant1_coord.append(x2)
+                    enfant2_coord.append(x1)
+            return Individu(enfant1_coord, self.fenetres, self.codage), \
+                Individu(enfant2_coord, self.fenetres, self.codage)
 
     def encoder(self):
         if self.codage:
