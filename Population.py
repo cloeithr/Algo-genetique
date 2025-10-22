c96bc0e73fd1b284aaef0ed50bd2a2a04a933a48
CLOE INTIHAR <cloeintihar24@gmail.com>
2025-10-22 10:57:02 +0200

    test sur population

--- Population.py
+++ Population.py
@@ -9,7 +9,7 @@ class Population:
         self.fenetres = afenetres
         self.fonction_objectif = afonction_objectif
         self.codage = acodage
-        self.individus = []
+        self.individus = [] 
 
     def initialiser(self):
         """Crée une population aléatoire d'individus."""
@@ -31,3 +31,33 @@ class Population:
     def remplacer(self, nouvel_individu, index):
         """Remplace un individu à une position donnée."""
         self.individus[index] = nouvel_individu  
+        
+
+if __name__ == '__main__':
+    from Individu import Individu
+    import random
+
+    # Fonction objectif simple
+    def fonction_test(x):
+        return sum([xi**2 for xi in x])  # minimisation de x1² + x2²
+
+    # Fenêtres de recherche pour chaque coordonnée
+    fenetres = [(-5, 5), (-5, 5)]
+
+    # Créer une population
+    pop = Population(ataille=5, afenetres=fenetres, afonction_objectif=fonction_test)
+
+    # Initialiser les individus
+    pop.initialiser()
+    print("Population initiale :")
+    for ind in pop.individus:
+        print(ind)
+
+    # Évaluer tous les individus
+    pop.evaluer()
+    print("\nPopulation après évaluation :")
+    for ind in pop.individus:
+        print(ind)
+
+    # Trouver le meilleur individu
+    print("\nMeilleur individu :", pop.meilleur())
