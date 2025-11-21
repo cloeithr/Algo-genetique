74a6ef51855e7696a5248a9ccfecbfaacebc850b
FatimaZahra00126 <bouzagoufatimazahra@gmail.com>
2025-11-21 10:54:25 +0100

    modif

--- Population.py
+++ Population.py
@@ -6,28 +6,22 @@ Description :
         - génération initiale
         - évaluation
         - sélection du meilleur
-        - remplacement d'individus
+        - remplacement
 """
 
+import random
 from Individu import Individu
 from Fenetre import Fenetre
-from Performance import Performance
-import random
-
-
-
-
 
 
 class Population:
-    """Représente l'ensemble d'une population d'individus."""
+    """Représente une population d'individus."""
 
     def __init__(self, aTaille, aFenetres, aFonctionObjectif, aCodage=None):
         """
-        Constructeur.
-        :param aTaille: nombre d'individus dans la population
+        :param aTaille: nombre total d'individus
         :param aFenetres: liste d'objets Fenetre
-        :param aFonctionObjectif: fonction f(x) à minimiser
+        :param aFonctionObjectif: fonction f(x) à optimiser
         :param aCodage: stratégie de codage (optionnelle)
         """
         self.taille = aTaille
@@ -35,70 +29,84 @@ class Population:
         self.fonction_objectif = aFonctionObjectif
         self.codage = aCodage
 
-        self.individus = []  # sera rempli dans initialiser()
+        self.individus = []  # sera rempli par initialiser()
 
- 
+
+    # CRÉATION POPULATION
+
 
     def initialiser(self):
-        """Crée une population initiale aléatoire."""
-        self.individus = []
+        """Génère une population initiale aléatoire."""
+        self.individus = [
+            Individu(self.fenetres, self.codage)
+            for _ in range(self.taille)
+        ]
 
-        for _ in range(self.taille):
-            ind = Individu(self.fenetres, self.codage)
-            self.individus.append(ind)
 
+    # ÉVALUATION
 
 
     def evaluer(self):
-        """Évalue tous les individus de la population."""
+        """Évalue tous les individus."""
         for ind in self.individus:
             ind.evaluer(self.fonction_objectif)
 
 
+    # MEILLEUR INDIVIDU
+
+
     def meilleur(self):
-        """Retourne l'individu avec la meilleure performance (minimisation)."""
+        """Retourne l'individu avec la plus petite performance."""
         return min(self.individus, key=lambda ind: ind.performance)
 
 
+    # REMPLACEMENT
+
 
     def remplacer(self, index, nouvel_individu):
-        """
-        Remplace un individu de la population à l'index donné.
-        """
+        """Remplace un individu à l'index donné."""
         self.individus[index] = nouvel_individu
 
 
+    # AFFICHAGE
+    
 
     def __str__(self):
         return "\n".join(str(ind) for ind in self.individus)
 
 
+# TEST LOCAL (MAIN)
 
-# TEST LOCAL 
 
 if __name__ == "__main__":
     print("=== Test de la classe Population ===")
 
-
-
-    # Création de 2 fenêtres pour les coordonnées
+    # 1) Création de fenêtres
     fen1 = Fenetre("x1", -5, 5)
     fen2 = Fenetre("x2", -5, 5)
     fenetres = [fen1, fen2]
 
-    # Population de 5 individus
-    pop = Population(aTaille=5, aFenetres=fenetres, aFonctionObjectif=Performance.evaluate)
+    # 2) Fonction objectif simple
+    def f_test(x):
+        return sum(xi ** 2 for xi in x)
 
-    # Initialisation
-    print("\nInitialisation :")
+    # 3) Création population
+    pop = Population(
+        aTaille=5,
+        aFenetres=fenetres,
+        aFonctionObjectif=f_test
+    )
+
+    # 4) Initialisation
+    print("\n--- Initialisation ---")
     pop.initialiser()
     print(pop)
 
-    # Évaluation
-    print("\nÉvaluation :")
+    # 5) Évaluation
+    print("\n--- Évaluation ---")
     pop.evaluer()
     print(pop)
 
-    # Meilleur individu
-    print("\nMeilleur individu :")
+    # 6) Meilleur individu
+    print("\n--- Meilleur individu ---")
     print(pop.meilleur())
